db3294e7aa1f5e57380bfe49d0cf868c4ffefa4c
Marbelous <maksimikk@gmail.com>
2018-09-27 17:06:08 -0400

    Added basic Stack methods.

--- Stack/Program.cs
+++ Stack/Program.cs
@@ -6,7 +6,62 @@ namespace Stack
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+
+
+
+            Console.ReadKey();
+        }
+
+        public class Stack
+        {
+            private int maxSize;
+            private string[] stackArray;
+            private int top;
+
+            public Stack(int size)
+            {
+                maxSize = size;
+                stackArray = new string[maxSize];
+                top = -1;  // Index of element at the top of the stack.
+            }
+            public void Push(string m)
+            {
+                if (isFull())
+                {
+                    Console.WriteLine("This stack is full!");
+                }
+                else
+                {
+                    top++;
+                    stackArray[top] = m;
+                }
+            }
+
+            public string Pop()
+            {
+                if (isEmpty())
+                {
+                    Console.WriteLine("This stack is empty!");
+                    return "---";
+                }
+                else
+                {
+                    int old_top = top; // Store temporary value of top.
+                    top--;
+                    return stackArray[old_top];
+                }
+            }
+
+            private bool isFull()
+            {
+                return (maxSize - 1 == top);
+            }
+
+            private bool isEmpty()
+            {
+                return (top == -1);
+            }
         }
     }
 }
